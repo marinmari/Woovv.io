11f054f79ae2fa7d36b62a69ae1ec140f6b63e4b
jonathan justman <jojo.justman@gmail.com>
2021-09-08 20:17:31 +0200

    feature-seed-ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,31 +4,36 @@ Coworking.destroy_all
 User.destroy_all 
 Company.destroy_all
 
-# ['coworkings', 'users', 'companies'].map {|tab| ActiveRecord::Base.connection.reset_pk_sequence!(tab)}
+['coworkings', 'users', 'companies'].map {|tab| ActiveRecord::Base.connection.reset_pk_sequence!(tab)}
 
 bdd = CSV.read("db/db_urls.csv")
 
-# 50.times do |i|
-#   User.create(email:"cowork#{i}@coworking.com", password:'azerty')
-# end
+10.times do |i|
+  User.create(email:"cowork#{i}@coworking.com", password:'azerty')
+end
 
-# 10.times do |i|
-#   Company.create(legal_name:"Company-#{i}")
-# end
+10.times do |i|
+  Company.create(legal_name:"Company-#{i}")
+end
 
-# bdd.each do |row|
-#   row_2 = row[0].split(',')
+bdd.each do |row|
+  row_2 = row[0].split(",")
+  # puts row_2[0][2..-2]
+  new_coworking = Coworking.new
+begin
+  new_coworking.name = row_2[0][2..-2]
+  new_coworking.description = Faker::Lorem.sentence
+  new_coworking.address=row_2[3][2..-2]
+  new_coworking.city=row_2[5][1..-2]
+  new_coworking.zipcode=row_2[4][1..-2]
+  new_coworking.country="France"
+  new_coworking.manager_id = rand(1..10)
+  new_coworking.managing_company_id = rand(1..10)
+  # (new_coworking.is_from_scrapping?) 
+rescue
+end
+  new_coworking.save
 
-#   Coworking.create({
-#     name: (row_2[0])[2..-2],
-#     description: Faker::Lorem.sentence,
-#     # address:(row_2[3])[1..-2],
-#     # city:(row_2[5])[1..-2],
-#     # zipcode:(row_2[4])[1..-2],
-#     country:"France",
-#     manager_id:rand(1..10),
-#     is_from_scrapping?: true
-#   })
-# end
+end
 
 
