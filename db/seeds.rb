95bda2ed4b3c563bb80d4535e44032682bbef698
jonathan justman <jojo.justman@gmail.com>
2021-09-08 18:51:05 +0200

    feature-seed-in-progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+Coworking.destroy_all
+User.destroy_all 
+Company.destroy_all
+
+# ['coworkings', 'users', 'companies'].map {|tab| ActiveRecord::Base.connection.reset_pk_sequence!(tab)}
+
+bdd = CSV.read("db/db_urls.csv")
+
+# 50.times do |i|
+#   User.create(email:"cowork#{i}@coworking.com", password:'azerty')
+# end
+
+# 10.times do |i|
+#   Company.create(legal_name:"Company-#{i}")
+# end
+
+# bdd.each do |row|
+#   row_2 = row[0].split(',')
+
+#   Coworking.create({
+#     name: (row_2[0])[2..-2],
+#     description: Faker::Lorem.sentence,
+#     # address:(row_2[3])[1..-2],
+#     # city:(row_2[5])[1..-2],
+#     # zipcode:(row_2[4])[1..-2],
+#     country:"France",
+#     manager_id:rand(1..10),
+#     is_from_scrapping?: true
+#   })
+# end
+
+
