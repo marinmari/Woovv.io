9721ca37f9969c4f563c9990e33514990bc926d8
Marin <marin.chretien@gmail.com>
2021-09-08 18:37:23 +0200

    chore(model): delete all validation on booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,4 @@ class Booking < ApplicationRecord
   belongs_to :booking_status
   belongs_to :coworking
   belongs_to :coworker, class_name: "User"
-  # validates :start_date, presence: true 
-  # validates :end_date, presence: true 
-  validates :coworking_rules_approved, presence: true, acceptance: true
 end
