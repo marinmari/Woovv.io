9db84db1b419568eca6bafc17c5bd7943a7dcec5
Marin <marin.chretien@gmail.com>
2021-09-09 18:17:24 +0200

    feature(Admin): add forest admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount ForestLiana::Engine => '/forest'
   resources :availabilities
   resources :bookings
   resources :companies
