e25cfe8bbd083246cda23e79fec861c9034ca10c
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-09-10 16:55:55 +0200

    features(views): show coworking add photos

--- app/controllers/coworking_pictures_controller.rb
+++ app/controllers/coworking_pictures_controller.rb
@@ -4,4 +4,9 @@ class CoworkingPicturesController < ApplicationController
     @coworking.coworking_pictures.attach(params[:coworking_pictures])
     redirect_to(coworking_path(@coworking))
   end
+  def destroy
+    @coworking = Coworking.find(params[:coworking_id])
+    @coworking.coworking_pictures.find(params[:id]).destroy
+    redirect_to(coworking_path(@coworking))
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "static_pages#home"
-  resources :coworkings, only: [:index, :show]
+  resources :coworkings, only: [:index, :show] do
+    resources :coworking_pictures, only: [:create, :destroy]
+  end
   resources :users, only: [:edit]
 end
