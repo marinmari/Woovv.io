cf2a112c3fae03614f1e8eb887f3c138e645d474
Marin <marin.chretien@gmail.com>
2021-09-10 15:09:53 +0200

    bugfix(API): fix the use of Sengrid on woovv-staging

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-# Load the Rails application.
+# Loa
+ the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_LOGIN'],
-  password: ENV['SENDGRID_PWD'],
-  domain: 'woovv-staging.herokuapp.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'woovv-staging.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
