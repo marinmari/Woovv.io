356b0488978538a13643a903bb3979842374ff71
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-09-13 22:12:34 +0200

    avatar user & view show user

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "static_pages#home"
-
   get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
+  get '/cgv', to: 'static_pages#cgv'
+  get '/help', to: 'static_pages#help'
   resources :coworkings, only: [:index, :show] do
     resources :coworking_pictures, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit] do 
+    resources :avatars, only: [:create, :destroy]
+  end 
 end
