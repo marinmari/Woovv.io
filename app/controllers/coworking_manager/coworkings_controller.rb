20468d74bd544c6ac572484e08e14cc2b72b220c
David Guetta <davguetta@gmail.com>
2021-09-14 16:32:01 +0200

    feature(edit): add coworking form and private set up form in Coworking Edit

--- app/controllers/coworking_manager/coworkings_controller.rb
+++ app/controllers/coworking_manager/coworkings_controller.rb
@@ -7,13 +7,23 @@ class CoworkingManager::CoworkingsController < ApplicationController
   end
 
   def edit
-    @coworking = Coworking.where(coworking_manager: current_user).find(params[:id])
+    set_coworking
   end
 
   def update
     puts "😎"*60
-    puts params    
+    puts params
+    puts params[:coworking]
+    puts params[:private_set_up]
     puts "😎"*60
+    set_coworking
+    unless coworking_params == nil
+      @coworking.update(coworking_params)
+    end
+    unless private_set_up_params == nil
+      @private_set_up.update(private_set_up_params)
+    end
+    redirect_to edit_coworking_manager_coworking_path(params[:id])
   end
 
   private 
@@ -29,4 +39,22 @@ class CoworkingManager::CoworkingsController < ApplicationController
       redirect_to coworking_manager_coworkings_path
     end
   end
+
+  def set_coworking
+    @coworking = Coworking.where(coworking_manager: current_user).find(params[:id])
+    @private_set_up = @coworking.private_set_up
+  end
+
+  def coworking_params
+    unless params[:coworking] == nil
+      params.require(:coworking).permit(:name, :description, :phone_number, :email)
+    end
+  end
+
+  def private_set_up_params
+    unless params[:private_set_up] == nil
+      params.require(:private_set_up).permit(:wifi_password, :manager_phone_number)
+    end
+  end
+
 end
