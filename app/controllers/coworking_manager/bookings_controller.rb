e53e2b48d3827381a3ef4f175a5f62241860ff94
Marin <marin.chretien@gmail.com>
2021-09-16 23:49:01 +0200

    feat(bookings): adding coworking_manager bookings index view (in process)

--- app/controllers/coworking_manager/bookings_controller.rb
+++ app/controllers/coworking_manager/bookings_controller.rb
@@ -1,10 +1,10 @@
 class CoworkingManager::BookingsController < ApplicationController
   
-  before_action :is_booking_manager?
-  before_action :manages_this_booking?, only: [:edit]
+  # before_action :is_booking_manager?
+  # before_action :manages_this_booking?, only: [:edit]
   
   def index
-    @bookings = booking.where(coworking_manager: current_user)
+    @bookings = current_user.managed_coworkings.bookings
   end 
 
   def create
@@ -15,22 +15,22 @@ class CoworkingManager::BookingsController < ApplicationController
 
 
   private 
-  def is_booking_manager?
-    if Booking.where(coworking_manager: current_user).count < 1 
-      redirect_to root_path
-    end
-  end
+  # def is_booking_manager?
+  #   if Booking.where(coworking_manager: current_user).count < 1 
+  #     redirect_to root_path
+  #   end
+  # end
 
-  def manages_this_booking?
-    unless Booking.where(coworking_manager: current_user).include? Booking.find(params[:id])
-      redirect_to coworking_manager_bookings_path
-    end
-  end
+  # def manages_this_booking?
+  #   unless Booking.where(coworking_manager: current_user).include? Booking.find(params[:id])
+  #     redirect_to coworking_manager_bookings_path
+  #   end
+  # end
 
-  def booking_params
-    unless params[:booking] == nil
-      params.require(:booking).permit(:start_date, :end_date)
-    end
-  end
+  # def booking_params
+  #   unless params[:booking] == nil
+  #     params.require(:booking).permit(:start_date, :end_date)
+  #   end
+  # end
 
 end
---
+++ app/views/coworking_manager/bookings/index.htlml.erb
@@ -0,0 +1,1 @@
+<h1>Bookings</h1>
