21d1832444397b60f38d33e9c41308d265aa2f1c
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-09-16 12:16:57 +0200

    fix map in index & show coworking

--- app/controllers/coworkings_controller.rb
+++ app/controllers/coworkings_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CoworkingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_coworking, only: %i[show edit update destroy]
