f14bbab5fa4d3f1dda8edc2a808f8ce75f6f63bf
jonathan justman <jojo.justman@gmail.com>
2021-09-17 15:56:37 +0200

    mailer booking_request + booking_update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,12 +5,22 @@ class Booking < ApplicationRecord
   belongs_to :coworking
   belongs_to :coworker, class_name: "User"
 
-  after_create :send_confirmation_email
+  after_create :send_request_confirmation_emails
+  after_update :send_booking_response_email
 
-  
 
-  def send_confirmation_email
-    BookingMailer.booking_confirmation_email_manager(self).deliver_now
+  def send_request_confirmation_emails
+    BookingMailer.booking_request_email_manager(self).deliver_now
+    BookingMailer.booking_request_email_coworker(self).deliver_now
+  end
+
+  def send_booking_response_email
+    if self.booking_status_id == 2
+      BookingMailer.booking_request_confirmation(self).deliver_now
+    elsif self.booking_status_id == 3
+      BookingMailer.booking_request_rejection(self).deliver_now
+    end
+
   end
 
 end
