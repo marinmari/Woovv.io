beecd912af0cfd3cb50f70b6fdcb44bb0daedc8f
jonathan justman <jojo.justman@gmail.com>
2021-09-17 17:37:20 +0200

    minor changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,15 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.create(email:params[:email], content:params[:content], object:params[:object])
     if @contact.save
-      puts "😆 success 😆"
+      flash[:success] = "Votre email a bien été envoyé à la team Woovv ! 😘"
       redirect_to root_path
     else
       puts "😡 echec 😡"
       redirect_to root_path
     end
   end
+
+  private
+
+
 end
