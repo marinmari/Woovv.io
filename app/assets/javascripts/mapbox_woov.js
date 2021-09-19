ac53416f8c78e10a5e11a92eaf3e996fd4fe4b7e
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-09-19 19:36:36 +0200

    fix view show CW, clean code js, allow research in DOMTOM, add zoom to maps

--- app/assets/javascripts/mapbox_woov.js
+++ app/assets/javascripts/mapbox_woov.js
@@ -3,75 +3,39 @@ window.onload = function () {
   const loaderElement = document.getElementById('loader');
   const mapElement = document.getElementById('map')
   const geoElement = document.getElementById('geocoder')
-  
-
-
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   } else {
     mapboxgl.accessToken = geoElement.dataset.mapboxApiKey;
   }
-  var coordinates = document.getElementById("map")
-  if (!coordinates) {
-    const geocoder = new MapboxGeocoder({
-      // Initialize the geocoder
-      accessToken: mapboxgl.accessToken, // Set the access token
-      mapboxgl: mapboxgl, // Set the mapbox-gl instance
-      placeholder: 'Rechercher un coworking', // Placeholder text for the search bar
-      zoom: 12,
-      types: 'country,region,place,postcode,locality,address',
-      countries: "fr"
-    });
+  
+// code map for landing page (begin here)
+  if (!mapElement && geoElement) {
+    mapboxgl.accessToken = geoElement.dataset.mapboxApiKey;
+    const results = document.getElementById('result')
+    console.log("je suis dans la landing page")
+    const geocoder = initGeocoder(12) //value of zoom as argument
     geocoder.addTo('#geocoder');
-      
-      // Get the geocoder results container.
-      const results = document.getElementById('result');
       // Add geocoder result to container.
       geocoder.on('result', (e) => {
-        // var tag = document.createElement('<form accept-charset="UTF-8" action="/" method="get"><input name="authenticity_token" type="hidden" value="J7CBxfHalt49OSHp27hblqK20c9PgwJ108nDHX/8Cts="/><input name="geocode_information" type="hidden"value="'+JSON.stringify(e.result, null, 2)+'"/></form>');
-        let divFormu = document.createElement('div')
-        let formuTest = document.createElement('form')
-        let inputTest = document.createElement('input')
-        let inputGeo = document.createElement('input')
-        // let btnGeo = document.createElement('input')
-        divFormu.appendChild(formuTest)
-        formuTest.action = '/coworkings'
-        formuTest.method ='GET'
-        formuTest.id="jsform"
-        inputTest.name="authenticity_token"
-        inputTest.type="hidden"
-        inputTest.value=""
-        inputGeo.name ="geocode_information"
-        inputGeo.type = "hidden"
-        inputGeo.value= JSON.stringify(e.result, null, 2)
-        // btnGeo.type = "submit"
-        // btnGeo.value = "Valider"
-        // btnGeo.name = "btnGeo"
-        // btnGeo.className ="btn btn-primary search-btn"
-        formuTest.appendChild(inputTest)
-        formuTest.appendChild(inputGeo)
-        // formuTest.appendChild(btnGeo)
-        results.innerHTML += divFormu.innerHTML
-        document.getElementById('jsform').submit()
+        createForm(e, results)
       });
-      
       // Clear results container when search is cleared.
       geocoder.on('clear', () => {
       results.innerText = '';
       });
-  } else {
-    mapElement.style.visibility = "hidden";
-    geoElement.style.visibility = "hidden";
-
-    setTimeout(() => {
-      loaderElement.remove();
-      mapElement.style.visibility = "visible";
-      geoElement.style.visibility = "visible";
-    }, 2000);
-    if (document.getElementById("map").attributes[4]){
+      // code map for landing page (end here)
+  } else if(mapElement && geoElement) {
+      // code map for index coworking (begin here)
+    
+    console.log("je suis dans l'index CW'")
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    addLoader(loaderElement, mapElement, geoElement)
+    console.log("je suis dans l'index CW'")
+    
+    if (document.getElementById("map").attributes[4]){ //if information from geocoder
     var research = document.getElementById("map").attributes[4].value.replace('[', '').replace(']', '').split(', ').map(Number)
     }
-    // The value for 'accessToken' begins with 'pk...'
     var co_id = document.getElementById("map").attributes[2].value
     co_id = co_id.replace('[', '').replace(']', '').split(', ').map(Number)
     var coordinates = document.getElementById("map").attributes[1].value
@@ -80,7 +44,6 @@ window.onload = function () {
     coordinates = coordinates.split('], [').map(c => c.split(', ')).map(c => c.map(Number))
     var latitudes = coordinates.map(c => c[0]).filter(lat => lat !== 0)
     var longitudes = coordinates.map(c => c[1]).filter(lon => lon !== 0)
-    console.log(coordinates)
     if (research[0] !== 0 ) {
       var box_upper_lat = research[0] + 0.05
       var box_bottom_lat = research[0]- 0.05
@@ -105,11 +68,12 @@ window.onload = function () {
       center: [coordinates[0][0], coordinates[0][1]],
       zoom: 1
     });
+    map.addControl(new mapboxgl.NavigationControl()) //zoom +/- navigation
     // Set marker options.
     var group = []
-    if (coordinates.length > 1) {
       for (i = 0; i < coordinates.length; i++) {
         const newChildforPopup = document.getElementById('cwtoshow-' + co_id[i])
+
         const popup = new mapboxgl.Popup({ closeOnClick: true })
           .setLngLat([coordinates[i][0], coordinates[i][1]])
           .setHTML(newChildforPopup.innerHTML)
@@ -121,63 +85,19 @@ window.onload = function () {
           .addTo(map);
         group.push(marker);
       }
-    } else {
-      for (i = 0; i < coordinates.length; i++) {
-        const marker = new mapboxgl.Marker({
-          color: "#92DACA",
-          draggable: false,
-        }).setLngLat([coordinates[i][0], coordinates[i][1]])
-          .addTo(map);
-        group.push(marker)
-      }
-    }
     
     map.fitBounds([
       [box_upper_lat, box_right_lon], // southwestern corner of the bounds
       [box_bottom_lat, box_left_lon] // northeastern corner of the bounds
     ]);
-      
-      const geocoder2 = new MapboxGeocoder({
-        // Initialize the geocoder
-        accessToken: mapboxgl.accessToken, // Set the access token
-        mapboxgl: mapboxgl, // Set the mapbox-gl instance
-        placeholder: 'Rechercher un coworking', // Placeholder text for the search bar
-        marker:true,
-        zoom: 8,
-        types: 'country,region,place,postcode,locality,address',
-        countries: "fr"
-      });
+    const geocoder2 = initGeocoder(12)
       geocoder2.addTo('#geocoder');
         
         // Get the geocoder results container.
         const results = document.getElementById('result');
         // Add geocoder result to container.
         geocoder2.on('result', (e) => {
-          // var tag = document.createElement('<form accept-charset="UTF-8" action="/" method="get"><input name="authenticity_token" type="hidden" value="J7CBxfHalt49OSHp27hblqK20c9PgwJ108nDHX/8Cts="/><input name="geocode_information" type="hidden"value="'+JSON.stringify(e.result, null, 2)+'"/></form>');
-          let divFormu = document.createElement('div')
-          let formuTest = document.createElement('form')
-          let inputTest = document.createElement('input')
-          let inputGeo = document.createElement('input')
-          // let btnGeo = document.createElement('input')
-          divFormu.appendChild(formuTest)
-          formuTest.action = '/coworkings'
-          formuTest.method ='GET'
-          formuTest.id="jsform"
-          inputTest.name="authenticity_token"
-          inputTest.type="hidden"
-          inputTest.value=""
-          inputGeo.name ="geocode_information"
-          inputGeo.type = "hidden"
-          inputGeo.value= JSON.stringify(e.result, null, 2)
-          // btnGeo.type = "submit"
-          // btnGeo.value = "Valider"
-          // btnGeo.name = "btnGeo"
-          // btnGeo.className ="btn btn-primary search-btn"
-          formuTest.appendChild(inputTest)
-          formuTest.appendChild(inputGeo)
-          // formuTest.appendChild(btnGeo)
-          results.innerHTML += divFormu.innerHTML
-          document.getElementById('jsform').submit()
+          createForm(e, results)
         });
         
         // Clear results container when search is cleared.
@@ -188,6 +108,88 @@ window.onload = function () {
           color: "#008080",
           draggable: false,
         }).setLngLat(research)
-          .addTo(map)    
+          .addTo(map)
+        // code map for index coworking (end here)  
+        console.log(coordinates)  
+        //41 -51 max longitude -5, 9 min max latitude
+  }else{
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    // code map for show coworking (begin here)
+    var co_id = document.getElementById("map").attributes[2].value
+    co_id = co_id.replace('[', '').replace(']', '').split(', ').map(Number)
+    var coordinates = document.getElementById("map").attributes[1].value
+    var box_focus = document.getElementById("map").attributes[3].value
+    coordinates = coordinates.replace('[[', '').replace(']]', '')
+    coordinates = coordinates.split('], [').map(c => c.split(', ')).map(c => c.map(Number))
+    var latitudes = coordinates.map(c => c[0]).filter(lat => lat !== 0)
+    var longitudes = coordinates.map(c => c[1]).filter(lon => lon !== 0)
+    console.log(coordinates)
+    const map = new mapboxgl.Map({
+      container: 'map',
+      // Replace YOUR_STYLE_URL with your style URL.
+      style: 'mapbox://styles/amelieloulergue/ckt8hlgfo20tr19v18ez6bpe3',
+      center: [coordinates[0][0], coordinates[0][1]],
+      zoom: 16
+    });
+    map.addControl(new mapboxgl.NavigationControl()) //zoom +/- navigation
+    // Set marker options.
+    var group = []
+      for (i = 0; i < coordinates.length; i++) {
+        const marker = new mapboxgl.Marker({
+          color: "#92DACA",
+          draggable: false,
+        }).setLngLat([coordinates[i][0], coordinates[i][1]])
+          .addTo(map);
+        group.push(marker);
+      }
+      map.fitBounds([
+        [box_upper_lat, box_right_lon], // southwestern corner of the bounds
+        [box_bottom_lat, box_left_lon] // northeastern corner of the bounds
+      ]);
+    
   }
+  // code map for show coworking (end here)
+}
+
+function createForm(e, results){
+  let divFormu = document.createElement('div')
+  let formuTest = document.createElement('form')
+  let inputTest = document.createElement('input')
+  let inputGeo = document.createElement('input')
+  divFormu.appendChild(formuTest)
+  formuTest.action = '/coworkings'
+  formuTest.method ='GET'
+  formuTest.id="jsform"
+  inputTest.name="authenticity_token"
+  inputTest.type="hidden"
+  inputTest.value=""
+  inputGeo.name ="geocode_information"
+  inputGeo.type = "hidden"
+  inputGeo.value= JSON.stringify(e.result, null, 2)
+  formuTest.appendChild(inputTest)
+  formuTest.appendChild(inputGeo)
+  results.innerHTML += divFormu.innerHTML
+  document.getElementById('jsform').submit()
+}
+
+function initGeocoder(zoom){
+  return new MapboxGeocoder({
+    // Initialize the geocoder
+    accessToken: mapboxgl.accessToken, // Set the access token
+    mapboxgl: mapboxgl, // Set the mapbox-gl instance
+    placeholder: 'Rechercher un coworking', // Placeholder text for the search bar
+    zoom: zoom,
+    types: 'country,region,place,postcode,locality,address',
+    countries: "fr"
+  });
+}
+function addLoader(loaderElement, mapElement,geoElement){
+mapElement.style.visibility = "hidden";
+geoElement.style.visibility = "hidden";
+
+setTimeout(() => {
+  loaderElement.remove();
+  mapElement.style.visibility = "visible";
+  geoElement.style.visibility = "visible";
+}, 2000);
 }
